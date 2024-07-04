510afd2f3cb96cdcddb2d7f9c1b8aaf237bf34d8
vera_chernevich <9111402516v@gmail.com>
2024-07-04 08:14:26 +0300

    Task 16

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        int target = 5;
+        int res = solution.threeSumCloses(nums, target);
+        System.out.println(res);
+
+
+    }
+}
